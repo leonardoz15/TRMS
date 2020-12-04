abf9f6916cf5432d92d195790b137d48b87ca9fe
unknown <leonardoz@allegheny.edu>
2020-12-03 22:29:39 -0500

    template for reimbursement view

--- TRMS/src/main/resources/public/view-reimbursements.js
+++ TRMS/src/main/resources/public/view-reimbursements.js
@@ -9,6 +9,9 @@ window.onload = function () {
                 alert("Please login first");
                 window.location.replace("http://localhost:9091/login.html");
             }
+            else {
+                //generateViews()
+            }
         }
     }
 
@@ -17,3 +20,29 @@ window.onload = function () {
     //sends request
     xhrCheck.send();
 }
+
+
+
+
+
+
+
+//logout:
+var logout = document.getElementById("logout");
+
+logout.onclick = function() {
+
+    var out = new XMLHttpRequest();
+    var outUrl = "http://localhost:9091/logout";
+    out.onreadystatechange = function () {
+        if(out.readyState == 4 && out.status === 200) {
+            window.location.replace("http://localhost:9091/index.html");   
+        }
+    }
+
+    //opens up the request
+    out.open("GET", outUrl, true);
+    //sends request
+    out.send();
+
+};
