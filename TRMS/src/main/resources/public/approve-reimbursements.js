284a397839238dfa9d12493b178bc06e4cc42869
unknown <leonardoz@allegheny.edu>
2020-12-06 13:04:05 -0500

    working request view for approvals

--- TRMS/src/main/resources/public/approve-reimbursements.js
+++ TRMS/src/main/resources/public/approve-reimbursements.js
@@ -29,10 +29,21 @@ function getRequestsToApprove() {
     approveRequests.onreadystatechange = function () {
         if(approveRequests.readyState == 4 && approveRequests.status === 200) {
             let requestList = JSON.parse(approveRequests.responseText);
-
-
-
-            //generateRequests(id, type, cost, urgency)
+            requestList.forEach(element => {
+                let id = element.requestId;
+                let type = element.eventType;
+                let cost = element.cost;
+                let urgency = element.urgent;
+                switch(urgency) {
+                    case true:
+                        urgency = "URGENT";
+                        break;
+                    case false:
+                        urgency = "NOT URGENT";
+                        break;
+                }
+                generateRequests(id, type, cost, urgency);
+            });
         }
     }
     //opens up the request
@@ -45,6 +56,63 @@ function getRequestsToApprove() {
 
 //generate table of requests to approve
 function generateRequests(id, type, cost, urgency) {
+    //generate elements
+    let table = document.getElementById("requests-table");
+    let tableBody = document.createElement("tbody");
+    let tableRow = document.createElement("tr");
+    let idCol = document.createElement("td");
+    let typeCol = document.createElement("td");
+    let costCol = document.createElement("td");
+    let urgencyCol = document.createElement("td");
+    let actionsCol = document.createElement("td");
+    let detailsButton = document.createElement("button");
+    let approveButton = document.createElement("button");
+    let denyButton = document.createElement("button");
+
+    //style elements
+    idCol.className = "table-active";
+    typeCol.className = "table-active";
+    costCol.className = "table-active";
+    urgencyCol.className = "table-active";
+    actionsCol.className = "table-active";
+    detailsButton.type = "button";
+    approveButton.type = "button";
+    denyButton.type = "button";
+    detailsButton.className = "btn btn-outline-primary";
+    approveButton.className = "btn btn-outline-success";
+    denyButton.className = "btn btn-outline-danger";
+
+    //set values
+    idCol.innerHTML = "# "+id;
+    typeCol.innerHTML = type;
+    costCol.innerHTML = "$"+cost;
+    urgencyCol.innerHTML = urgency;
+    detailsButton.innerHTML = "Details";
+    approveButton.innerHTML = "Approve";
+    denyButton.innerHTML = "Deny";
+
+    //append elements
+    actionsCol.appendChild(detailsButton);
+    actionsCol.appendChild(approveButton);
+    actionsCol.appendChild(denyButton);
+    tableRow.appendChild(idCol);
+    tableRow.appendChild(typeCol);
+    tableRow.appendChild(costCol);
+    tableRow.appendChild(urgencyCol);
+    tableRow.appendChild(actionsCol);
+    tableBody.appendChild(tableRow);
+    table.appendChild(tableBody);
+
+    //add listeners to buttons
+    detailsButton.addEventListener("click", function() {
+        //window.location.replace("http://localhost:9091/details-reimbursement.html?request_id="+id);
+    })
+    approveButton.addEventListener("click", function () {
+        //approveRequest(id);
+    })
+    denyButton.addEventListener("click", function () {
+        //denyRequest(id);
+    })
 
 }
 
